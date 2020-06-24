49233a11a051b052ee8326a22e7dc1d470e7a9a1
zlqphp <zlqphp@163.com>
2020-06-24 16:20:47 +0800

    猫眼电影爬虫v1.0

---
+++ week01/cat_movie/movie_v1.0.py
@@ -0,0 +1,64 @@
+from bs4 import BeautifulSoup
+import requests
+import random
+import lxml.etree
+import pandas as pd
+
+
+
+def get_movie_info():
+    url = 'https://maoyan.com/board'
+    USER_AGENT_LIST = [
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+        "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+        "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SE 2.X MetaSr 1.0; SE 2.X MetaSr 1.0; .NET CLR 2.0.50727; SE 2.X MetaSr 1.0)",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
+    ]
+    user_agent = random.choice(USER_AGENT_LIST)
+    headers = {"user-agent": user_agent}
+    r = requests.get(url,headers=headers)
+    soup = BeautifulSoup(r.text,'lxml')
+    content = soup.find_all("div",{"class":"board-item-content"})
+    for one_content in content:
+        name = one_content.find("p", {"class": "name"}).text
+        releasetime = one_content.find("p",{"class": "releasetime"}).text
+        part_link = one_content.find('a')['href']
+        link = "https://maoyan.com"+ part_link
+        # link1 = one_content.find('a').get("href")
+        res = requests.get(link,headers=headers)
+        selector = lxml.etree.HTML(res.text)
+        type = selector.xpath("/html/body/div[3]/div/div[2]/div[1]/ul/li[1]/a/text()")[0]
+        # print(type)
+
+        text = name + "," + releasetime + "," + type + "\n"
+        # save_file(title)
+        save_file(text)
+
+
+def save_file(content):
+    with open("movie.csv",'a',encoding="utf-8") as f:
+        f.write(content)
+
+
+def main():
+    title = "名称,类型,上映时间" + "\n"
+    save_file(title)
+    get_movie_info()
+
+
+if __name__ == '__main__':
+    main()
